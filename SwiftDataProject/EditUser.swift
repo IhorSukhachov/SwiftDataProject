8002f25aba947ae841ff3964e30bbf407e60893c
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-10 13:41:39 +0200

    creating edit user  view

---
+++ SwiftDataProject/EditUser.swift
@@ -0,0 +1,18 @@
+//
+//  EditUser.swift
+//  SwiftDataProject
+//
+//  Created by Ihor Sukhachov on 10.12.2025.
+//
+
+import SwiftUI
+
+struct EditUser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditUser()
+}
