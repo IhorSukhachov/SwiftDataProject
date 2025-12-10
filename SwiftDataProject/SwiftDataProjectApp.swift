9540b9da63f3ee25e11026b2b9f240c0c3a6558f
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-10 12:08:56 +0200

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Ihor Sukhachov on 10.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
