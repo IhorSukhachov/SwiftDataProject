fe7e8f0847f8ec6cd5b4406a72d5b5ea2ffc5f7c
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-10 14:42:45 +0200

    adding sample users for testing sorting

--- SwiftDataProject/ContentView.swift
+++ SwiftDataProject/ContentView.swift
@@ -10,29 +10,40 @@ import SwiftUI
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @Query(sort: \User.name) var users: [User]
-    @State private var path = [User]()
+    //   @State private var path = [User]()
     var body: some View {
-        NavigationStack(path: $path) {
+        NavigationStack {
             List(users) {user in
                 NavigationLink(value: user) {
                     Text(user.name)
                 }
             }
             .navigationTitle("Users")
-            .navigationDestination(for: User.self) {user in
-                EditUserView(user: user)
-            }
-            .toolbar {
-                Button("Add User", systemImage: "plus") {
-                    let user = User(name: "", city: "", joinDate: .now)
-                    modelContext.insert(user)
-                    path = [user]
-                }
+            //            .navigationDestination(for: User.self) {user in
+            //                EditUserView(user: user)
+        }
+        .toolbar {
+            Button("Add samples", systemImage: "plus") {
+                //                    let user = User(name: "", city: "", joinDate: .now)
+                //                    modelContext.insert(user)
+                //                    path = [user]
+                try? modelContext.delete(model: User.self)
+                
+                let first = User(name: "Ihor", city: "Kiev", joinDate: .now.addingTimeInterval(86400 * -10)) //ten days in the psst
+                let second = User(name: "Sasha", city: "Poltava", joinDate: .now.addingTimeInterval(86400 * -5))
+                let third = User(name: "Oleg", city: "Lviv", joinDate: .now.addingTimeInterval(86400 * 5))
+                let forth = User(name: "Johny", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
+                
+                modelContext.insert(first)
+                modelContext.insert(second)
+                modelContext.insert(third)
+                modelContext.insert(forth)
             }
         }
-       
     }
+    
 }
+
 
 #Preview {
     ContentView()
