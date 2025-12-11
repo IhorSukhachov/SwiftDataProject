43dba903814b2a78682971302de6e33014df6e59
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-11 11:58:13 +0200

    creating users list in users view

--- SwiftDataProject/UsersView.swift
+++ SwiftDataProject/UsersView.swift
@@ -4,15 +4,20 @@
 //
 //  Created by Ihor Sukhachov on 11.12.2025.
 //
-
+import SwiftData
 import SwiftUI
 
 struct UsersView: View {
+    @Query  var users: [User]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(users) {user in
+            Text(user.name)
+        }
     }
 }
 
 #Preview {
     UsersView()
+        .modelContainer(for: User.self)
 }
