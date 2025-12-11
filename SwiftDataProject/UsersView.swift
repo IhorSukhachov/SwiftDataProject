e1480b83c5f5588c7e02d1deebb66d0e2ad5a8c9
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-11 12:37:44 +0200

    adding jobs count to user lists

--- SwiftDataProject/UsersView.swift
+++ SwiftDataProject/UsersView.swift
@@ -12,8 +12,24 @@ struct UsersView: View {
     
     var body: some View {
         List(users) {user in
-            Text(user.name)
+            HStack {
+                Text(user.name)
+                
+                Spacer()
+                
+                Text(String(user.jobs.count))
+                    .fontWeight(.black)
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .clipShape(.capsule)
+            }
+            
         }
+        
+        
+        
     }
     init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]) {
         _users = Query(filter: #Predicate<User> {user in
