98b0afd18e617f5075561d81103a16960b61a816
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-11 12:22:00 +0200

    add picker to select sorting order

--- SwiftDataProject/ContentView.swift
+++ SwiftDataProject/ContentView.swift
@@ -14,39 +14,53 @@ struct ContentView: View {
         SortDescriptor(\User.name),
         SortDescriptor(\User.joinDate)
     ]
-//    @Query(filter: #Predicate<User> {user in
-//        user.name.localizedStandardContains("o") &&
-//        user.city == "Kiev"
-//    }, sort: \User.name) var users: [User]
+    //    @Query(filter: #Predicate<User> {user in
+    //        user.name.localizedStandardContains("o") &&
+    //        user.city == "Kiev"
+    //    }, sort: \User.name) var users: [User]
     //   @State private var path = [User]()
     var body: some View {
         NavigationStack {
             UsersView(minimumJoinDate: showingUpcomingOnly ? .now : .distantPast, sortOrder: sortOrder )
-            .navigationTitle("Users")
+                .navigationTitle("Users")
             //            .navigationDestination(for: User.self) {user in
             //                EditUserView(user: user)
             
-            .toolbar {
-                Button("Add samples", systemImage: "plus") {
-                    //                    let user = User(name: "", city: "", joinDate: .now)
-                    //                    modelContext.insert(user)
-                    //                    path = [user]
-                    try? modelContext.delete(model: User.self)
+                .toolbar {
+                    Button("Add samples", systemImage: "plus") {
+                        //                    let user = User(name: "", city: "", joinDate: .now)
+                        //                    modelContext.insert(user)
+                        //                    path = [user]
+                        try? modelContext.delete(model: User.self)
+                        
+                        let first = User(name: "Ihor", city: "Kiev", joinDate: .now.addingTimeInterval(86400 * -10)) //ten days in the psst
+                        let second = User(name: "Sasha", city: "Poltava", joinDate: .now.addingTimeInterval(86400 * -5))
+                        let third = User(name: "Oleg", city: "Lviv", joinDate: .now.addingTimeInterval(86400 * 5))
+                        let forth = User(name: "Johny", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
+                        
+                        modelContext.insert(first)
+                        modelContext.insert(second)
+                        modelContext.insert(third)
+                        modelContext.insert(forth)
+                    }
+                    Button(showingUpcomingOnly ? "Show Everyone" : "Show Upcoming" ) {
+                        showingUpcomingOnly.toggle()
+                    }
                     
-                    let first = User(name: "Ihor", city: "Kiev", joinDate: .now.addingTimeInterval(86400 * -10)) //ten days in the psst
-                    let second = User(name: "Sasha", city: "Poltava", joinDate: .now.addingTimeInterval(86400 * -5))
-                    let third = User(name: "Oleg", city: "Lviv", joinDate: .now.addingTimeInterval(86400 * 5))
-                    let forth = User(name: "Johny", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
-                    
-                    modelContext.insert(first)
-                    modelContext.insert(second)
-                    modelContext.insert(third)
-                    modelContext.insert(forth)
+                    Picker("Sort", selection: $sortOrder) {
+                        Text("Sort by name")
+                            .tag([
+                                SortDescriptor(\User.name),
+                                
+                                SortDescriptor(\User.joinDate)
+                            ])
+                        Text("Sort by join date")
+                            .tag([
+                                SortDescriptor(\User.joinDate),
+                                SortDescriptor(\User.name)
+                            ])
+                    }
                 }
-                Button(showingUpcomingOnly ? "Show Everyone" : "Show Upcoming" ) {
-                    showingUpcomingOnly.toggle()
-                }
-            }
         }
     }
 }
